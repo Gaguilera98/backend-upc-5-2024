09ad348537a141ba33c71312ffe6ca1b072d77cf
aguilerafriasg461 <aguilerafriasg461@gmail.com>
2023-06-28 11:59:35 -0400

    modulo de habitaciones

---
+++ backend-upc-5-2023/Dominio/Habitaciones.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace backend_upc_5_2023.Dominio
+{
+    /// <summary>
+    /// Dominio de la tabla Usuarios
+    /// </summary>
+    public class Habitaciones
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+       
+
+        /// <summary>
+        /// Gets or sets the nombre completo.
+        /// </summary>
+        /// <value>
+        /// The nombre completo.
+        /// </value>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string Area { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Capacidad { get; set; }
+        public string Descripcion { get; set; }
+        //public string FechaRegistro { get; set;}
+       // public int EstadoRegristro { get; set; }
+
+        ////Email
+    }
+}
---
+++ backend-upc-5-2023/Servicios/HabitacionesServicios.cs
@@ -0,0 +1,67 @@
+﻿// Ignore Spelling: Categoria
+
+using backend_upc_5_2023.Connection;
+using backend_upc_5_2023.Dominio;
+using Dapper;
+using System.Data;
+
+namespace backend_upc_5_2023.Servicios
+{
+    /// <summary>
+    /// Clase de servicios para la entidad: <see cref="Habitaciones"/>
+    /// </summary>
+    public static class HabitacionesServicios
+    {
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="System.Data.SqlClient.SqlException"></exception>
+        public static IEnumerable<T> Get<T>()
+        {
+            const string sql = "SELECT * FROM HABITACION WHERE ESTADO_REGISTRO = 1";
+
+            return DBManager.Instance.GetData<T>(sql);
+        }
+
+        /// <summary>
+        /// Gets the by identifier.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public static T GetById<T>(int id)
+        {
+            const string sql = "SELECT * FROM HABITACION WHERE ID = @Id AND ESTADO_REGISTRO = 1";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("ID", id, DbType.Int64);
+
+            var result = DBManager.Instance.GetDataConParametros<T>(sql, parameters);
+
+            return result.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Inserts the specified usuarios.
+        /// </summary>
+        /// <param name="usuarios">The usuarios.</param>
+        /// <returns></returns>
+        /// <exception cref="System.Data.SqlClient.SqlException"></exception>
+        public static int Insert(Habitaciones habitaciones)
+        {
+            const string sql = "INSERT INTO HABITACION([NOMBRE], [Area], [CAPACIDAD],[DESCRIPCION]) VALUES (@Nombre, @Area, @Capacidad,@Descripcion) ";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("Nombre", habitaciones.Nombre, DbType.String);
+            parameters.Add("Area", habitaciones.Area, DbType.String);
+            parameters.Add("Capacidad", habitaciones.Capacidad, DbType.String);
+            parameters.Add("Descripcion", habitaciones.Descripcion, DbType.String);
+
+            var result = DBManager.Instance.SetData(sql, parameters);
+
+            return result;
+        }
+    }
+}
